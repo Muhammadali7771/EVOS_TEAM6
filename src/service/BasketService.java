08b570bcdd7d26d9c3c8eae7fd7970969336ad2d
Sherzod <nurboevsherzod4@gmail.com>
2024-03-11 18:10:50 +0500

    addGetMyBasket has been added

--- src/service/BasketService.java
+++ src/service/BasketService.java
@@ -2,11 +2,29 @@ package service;
 
 import model.Basket;
 
+import java.util.UUID;
+
 public class BasketService {
     private Basket[] baskets = new Basket[100];
     private int index = 0;
     public void addBasket(Basket basket){
         baskets[index++] = basket;
+    }
+    public Basket[] getMyBasket(UUID userId){
+        int count = 0;
+        for (int i=0; i < index; i++){
+            if (baskets[i] != null && baskets[i].getUserId().equals(userId) && baskets[i].isActive()){
+                count++;
+            }
+        }
+        Basket[] myBasket = new Basket[count];
+        int index1 = 0;
+        for (int i=0; i < index; i++){
+            if (baskets[i] != null && baskets[i].getUserId().equals(userId) && baskets[i].isActive()){
+                myBasket[index1++] = baskets[i];
+            }
+        }
+        return myBasket;
     }
 
 }
