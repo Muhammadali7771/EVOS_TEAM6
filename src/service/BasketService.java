cc8fdf743e6519aa5e794736da3911713c3ac348
Sherzod <nurboevsherzod4@gmail.com>
2024-03-11 17:58:58 +0500

    addBasket has been added

--- src/service/BasketService.java
+++ src/service/BasketService.java
@@ -5,5 +5,8 @@ import model.Basket;
 public class BasketService {
     private Basket[] baskets = new Basket[100];
     private int index = 0;
+    public void addBasket(Basket basket){
+        baskets[index++] = basket;
+    }
 
 }
