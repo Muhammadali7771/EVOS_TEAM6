ae030b431f543326f16ad97daf2d912aec5f2a29
Shahriddin <@sobirovshahriddin>
2024-03-11 14:13:30 +0500

    I added a method called addOrder to orderService

--- src/service/OrderService.java
+++ src/service/OrderService.java
@@ -2,8 +2,14 @@ package service;
 
 import model.Order;
 
+import java.util.UUID;
+
 public class OrderService {
     private Order[] orders = new Order[100];
     private int index = 0;
 
+    public void addOrder(Order order, UUID userId){
+        order.setProductId(userId);
+        orders[index++] = order;
+    }
 }
