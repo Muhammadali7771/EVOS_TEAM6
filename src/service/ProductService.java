dfa6118a87c8c54a16f5bfa91c00e4ea5a3050d2
Modern <sotimovf@mail.ru>
2024-03-11 20:39:59 +0500

    AddProduct and HasProduct have been added

--- src/service/ProductService.java
+++ src/service/ProductService.java
@@ -2,8 +2,24 @@ package service;
 
 import model.Product;
 
+import java.util.UUID;
+
 public class ProductService {
     private Product[] products = new Product[100];
     private int index = 0;
+   public boolean addProduct(Product product,UUID categoryId){
+       product.setCategoryId(categoryId);
+       products[index++]=product;
+       return  true;
+   }
+    public boolean hasProduct(String productName){
+       for (Product product:products){
+           if(product!=null||product.getName().equals(productName)){
+               return true;
+           }
+       }
+        return false;
+    }
+
 
 }
