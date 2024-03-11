11e977fb1dac83fe35e477b3457b5670e5acaa7b
Behzod <behzodjaloliddinov870@gmail.com>
2024-03-11 19:00:05 +0500

    UserService updated

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -23,4 +23,18 @@ public class UserService {
         return false;
     }
 
+    public User login(String userName) {
+        for (User user : users) {
+            if (user != null && user.getUsername().equals(userName)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public User[] getUsers() {
+        return users;
+    }
+
 }
+
